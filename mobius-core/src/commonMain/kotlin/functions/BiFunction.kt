9d516ce9590971d85fb0ce8bb610cce238a9cf08
Drew Carlson <DrewCarlson@users.noreply.github.com>
2019-05-08 12:02:38 -0700

    Add Function and BiFunction lambda factory operators

--- mobius-core/src/commonMain/kotlin/functions/BiFunction.kt
+++ mobius-core/src/commonMain/kotlin/functions/BiFunction.kt
@@ -3,5 +3,14 @@ package kt.mobius.functions
 
 /** Interface for simple functions with two arguments.  */
 interface BiFunction<T, U, R> {
+
+  companion object {
+    inline operator fun <T, U, R> invoke(
+      crossinline function: (@ParameterName("value1") T, @ParameterName("value2") U) -> R
+    ) = object : BiFunction<T, U, R> {
+      override fun apply(value1: T, value2: U): R = function(value1, value2)
+    }
+  }
+
   fun apply(value1: T, value2: U): R
 }
