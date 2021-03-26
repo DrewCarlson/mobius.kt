225a10bb6c03e2fce89e07a992d52d382a4ce985
Drew Carlson <awcarlson@protonmail.com>
2021-03-26 14:35:50 -0700

    Anonymous eventConsumer object
    This reverts commit e62f767392d58dc15ef42d804fb4cc4a5e83648b.

--- mobius-core/src/commonMain/kotlin/MobiusLoop.kt
+++ mobius-core/src/commonMain/kotlin/MobiusLoop.kt
@@ -58,14 +58,14 @@ class MobiusLoop<M, E, F> private constructor(
         }
     })
 
-    private val eventProcessor = eventProcessorFactory.create(effectDispatcher, Consumer { model ->
+    private val eventProcessor = eventProcessorFactory.create(effectDispatcher) { model ->
         mpp.synchronized(modelObservers) {
             mostRecentModel = model
             for (observer in modelObservers) {
                 observer.accept(model)
             }
         }
-    })
+    }
     private val effectConsumer: Connection<F>
     private val eventSourceDisposable: Disposable
 
@@ -79,7 +79,11 @@ class MobiusLoop<M, E, F> private constructor(
     private var disposed: Boolean = false
 
     init {
-        val eventConsumer: Consumer<E> = Consumer { event -> dispatchEvent(event) }
+        val eventConsumer: Consumer<E> = object : Consumer<E> {
+            override fun accept(value: E) {
+                dispatchEvent(value)
+            }
+        }
 
         this.effectConsumer = effectHandler.connect(eventConsumer)
         this.eventSourceDisposable = eventSource.subscribe(eventConsumer)
--- mobius-core/src/commonMain/kotlin/functions/Consumer.kt
+++ mobius-core/src/commonMain/kotlin/functions/Consumer.kt
@@ -3,16 +3,8 @@ package kt.mobius.functions
 import kotlin.js.JsName
 
 /** Interface for consuming values. */
-interface Consumer<V> {
-    companion object {
-        inline operator fun <V> invoke(crossinline accept: (value: V) -> Unit): Consumer<V> {
-            return object : Consumer<V> {
-                override fun accept(value: V) = accept(value)
-            }
-        }
-    }
+fun interface Consumer<V> {
 
     @JsName("accept")
     fun accept(value: V)
 }
-
