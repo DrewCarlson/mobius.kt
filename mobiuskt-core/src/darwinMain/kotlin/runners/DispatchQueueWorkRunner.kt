482dbd7ae22ac14aadffaee5a598e6b7dddb7203
Drew Carlson <awcarlson@protonmail.com>
2021-12-13 10:15:00 -0800

    Restrict DispatchQueueWorkRunner usage to new mm

--- mobiuskt-core/src/darwinMain/kotlin/runners/DispatchQueueWorkRunner.kt
+++ mobiuskt-core/src/darwinMain/kotlin/runners/DispatchQueueWorkRunner.kt
@@ -10,6 +10,13 @@ public fun WorkRunners.fromDispatchQueue(dispatchQueue: dispatch_queue_t): WorkR
 public class DispatchQueueWorkRunner(
     private val dispatchQueue: dispatch_queue_t
 ) : WorkRunner {
+
+    init {
+        check(Platform.memoryModel == MemoryModel.EXPERIMENTAL) {
+            "Using DispatchQueueWorkRunner requires the experimental memory model.\nSee https://github.com/JetBrains/kotlin/blob/master/kotlin-native/NEW_MM.md"
+        }
+    }
+
     override fun post(runnable: Runnable) {
         dispatch_async(dispatchQueue, runnable::run)
     }
