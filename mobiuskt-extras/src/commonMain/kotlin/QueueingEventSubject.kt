dc2fc742abe39b55a4db09b9808f57fbdcf6ba16
Drew Carlson <awcarlson@protonmail.com>
2021-12-31 10:06:56 -0800

    Add QueueingEventSubject

---
+++ mobiuskt-extras/src/commonMain/kotlin/QueueingEventSubject.kt
@@ -0,0 +1,83 @@
+package kt.mobius.extras
+
+import kotlinx.atomicfu.locks.SynchronizedObject
+import kotlinx.atomicfu.locks.synchronized
+import kt.mobius.EventSource
+import kt.mobius.disposables.Disposable
+import kt.mobius.functions.Consumer
+
+/**
+ * An EventSource that can also consume events. If it has a subscriber, events will be immediately
+ * forwarded to that subscriber. If it doesn't have a subscriber, it will queue up events (up to the
+ * maximum capacity specified in the constructor), and forward all queued events to the next
+ * subscriber. Only a single subscription at a time is permitted.
+ */
+public class QueueingEventSubject<E : Any>(
+    private val capacity: Int
+) : SynchronizedObject(), EventSource<E>, Consumer<E> {
+    private enum class State {
+        NO_SUBSCRIBER, SUBSCRIBED
+    }
+
+    private val queue = ArrayList<E>(capacity)
+
+    // State and subscriber are accessed only in synchronized sections
+    private var state = State.NO_SUBSCRIBER
+    private var subscriber: Consumer<E>? = null
+
+    override fun subscribe(eventConsumer: Consumer<E>): Disposable {
+        lateinit var queued: List<E>
+
+        // Do not invoke consumer in synchronized section
+        synchronized(this) {
+            if (state == State.SUBSCRIBED) {
+                error("Only a single subscription is supported, previous subscriber is: $subscriber")
+            }
+            state = State.SUBSCRIBED
+            subscriber = eventConsumer
+            queued = ArrayList(queue)
+            queue.clear()
+        }
+        queued.forEach(eventConsumer::accept)
+        return Unsubscriber()
+    }
+
+    override fun accept(value: E) {
+        var consumerToInvoke: Consumer<E>? = null
+
+        // Do not invoke consumer in synchronized section
+        synchronized(this) {
+            when (state) {
+                State.NO_SUBSCRIBER -> {
+                    check(queue.size < capacity) {
+                        "Queue capacity exceeded, cannot queue $value"
+                    }
+                    queue.add(value)
+                }
+                State.SUBSCRIBED -> consumerToInvoke = subscriber
+            }
+        }
+        consumerToInvoke?.accept(value)
+    }
+
+    private fun unsubscribe() {
+        synchronized(this) {
+            state = State.NO_SUBSCRIBER
+            subscriber = null
+        }
+    }
+
+    private inner class Unsubscriber : SynchronizedObject(), Disposable {
+        private var disposed = false
+
+        override fun dispose() {
+            synchronized(this) {
+                if (disposed) {
+                    return
+                }
+                disposed = true
+                unsubscribe()
+            }
+        }
+    }
+}
---
+++ mobiuskt-extras/src/commonTest/kotlin/QueueingEventSubjectTest.kt
@@ -0,0 +1,94 @@
+package kt.mobius.extras
+
+import kt.mobius.RecordingConsumer
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+
+class QueueingEventSubjectTest {
+
+    private lateinit var eventSubject: QueueingEventSubject<String>
+    private lateinit var receiver: RecordingConsumer<String>
+
+    @BeforeTest
+    fun setUp() {
+        eventSubject = QueueingEventSubject(3)
+        receiver = RecordingConsumer()
+    }
+
+    @Test
+    fun shouldForwardEventsWhenSubscribed() {
+        eventSubject.subscribe(receiver)
+        eventSubject.accept("hey")
+        receiver.assertValues("hey")
+    }
+
+    @Test
+    fun shouldQueueEventsWhenNotSubscribed() {
+        eventSubject.accept("hi")
+        eventSubject.accept("ho")
+        eventSubject.accept("to the mines we go")
+        eventSubject.subscribe(receiver)
+        receiver.assertValues("hi", "ho", "to the mines we go")
+    }
+
+    @Test
+    fun shouldStopSendingEventsWhenSubscriptionDisposed() {
+        val subscription = eventSubject.subscribe(receiver)
+        eventSubject.accept("a")
+        eventSubject.accept("b")
+        subscription.dispose()
+        eventSubject.accept("don't want to see this")
+        receiver.assertValues("a", "b")
+    }
+
+    @Test
+    fun shouldOnlySupportASingleSubscriber() {
+        eventSubject.subscribe(receiver)
+        assertFailsWith<IllegalStateException> {
+            eventSubject.subscribe(RecordingConsumer())
+        }
+    }
+
+    @Test
+    fun shouldSupportUnsubscribeAndResubscribe() {
+        eventSubject.accept("a")
+        val subscription = eventSubject.subscribe(receiver)
+        eventSubject.accept("b")
+        subscription.dispose()
+        eventSubject.accept("c")
+        eventSubject.subscribe(receiver)
+        receiver.assertValues("a", "b", "c")
+    }
+
+    @Test
+    fun shouldThrowWhenCapacityExceeded() {
+        eventSubject.accept("a")
+        eventSubject.accept("b")
+        eventSubject.accept("c")
+        assertFailsWith<IllegalStateException> {
+            eventSubject.accept("noo, too many things")
+        }
+    }
+
+    @Test
+    fun unsubscribeShouldBeIdempotent() {
+        // given a subscription that has been disposed
+        val subscription1 = eventSubject.subscribe(receiver)
+        subscription1.dispose()
+
+        // and a new subscription has been created
+        val subscription2 = eventSubject.subscribe(receiver)
+
+        // when the first subscription is disposed again
+        subscription1.dispose()
+
+        // then the second subscription is not disposed
+        eventSubject.accept("a")
+
+        // and disposing the second subscription works
+        subscription2.dispose()
+        eventSubject.accept("b")
+        receiver.assertValues("a")
+    }
+}
