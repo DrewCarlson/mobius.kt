a56ab8ff231e3d7b4055021667c39cc58f5af813
Drew Carlson <awcarlson+git@protonmail.com>
2023-02-23 21:49:40 -0800

    Add MappedEffectHandler

---
+++ mobiuskt-extras/src/commonMain/kotlin/MappedEffectHandler.kt
@@ -0,0 +1,45 @@
+package kt.mobius.extras
+
+import kt.mobius.Connectable
+import kt.mobius.Connection
+import kt.mobius.functions.Consumer
+import kotlin.js.JsExport
+
+
+/**
+ * Creates a [Connectable] that delegates to [effectHandler] and maps the Effect
+ * and Events using [mapEffect] and [mapEvent].
+ */
+@Suppress("NON_EXPORTABLE_TYPE")
+@JsExport
+public class MappedEffectHandler<I, O, II, OO> constructor(
+    private val effectHandler: Connectable<II, OO>,
+    private val mapEffect: (I) -> II? = { null },
+    private val mapEvent: (OO) -> O? = { null }
+) : Connectable<I, O> {
+
+    override fun connect(output: Consumer<O>): Connection<I> {
+        val delegate = effectHandler.connect { value ->
+            mapEvent(value)?.let(output::accept)
+        }
+        return object : Connection<I> {
+            override fun accept(value: I) {
+                mapEffect(value)?.let(delegate::accept)
+            }
+
+            override fun dispose() {
+                delegate.dispose()
+            }
+        }
+    }
+}
+
+public inline fun <I, reified O, reified II, OO> Connectable<II, OO>.mapped(
+    noinline mapEffect: (I) -> II? = { it as? II },
+    noinline mapEvent: (OO) -> O? = { it as? O }
+): Connectable<I, O> =
+    MappedEffectHandler(
+        this,
+        mapEffect,
+        mapEvent
+    )
